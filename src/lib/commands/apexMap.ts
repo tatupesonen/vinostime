74225d231bee5fed98ca59136b1ff29b8a9cc523
Tatu Pesonen <tatupe360@hotmail.com>
2021-10-24 02:37:42 +0300

    feat: add option to toggle ephemeral on /map

--- src/lib/commands/apexMap.ts
+++ src/lib/commands/apexMap.ts
@@ -9,12 +9,20 @@ const ApexMapCommand = createCommand({
   name: 'map',
   type: COMMAND_TYPE.CHANNEL,
   description: 'Returns the current map in casual rotation for Apex Legends',
+  options: [
+    {
+      type: 'BOOLEAN',
+      name: 'show-to-others',
+      description: 'If set to true, will post the embed in the channel for others to see.',
+      required: false,
+    },
+  ],
   async execute(interaction, container) {
     // Get service
     const service = container.getByKey<IApexLegendsService>(
       DITypes.apexLegendsService
     );
-    const baseReply: InteractionReplyOptions = { ephemeral: true };
+    const baseReply: InteractionReplyOptions = { ephemeral: !interaction.options.getBoolean("show-to-others") ?? false };
     await interaction.deferReply(baseReply);
     try {
       const mapData = await service.getMapEmbed();
